34ec6dd1d873389d2ea959c5e70257177580b0ed
Richard Strassheim <richard.strassheim@continental-corporation.com>
2020-01-14 13:50:18 +0100

    removed debug code

--- src/de/thm/project/chat/Main.java
+++ src/de/thm/project/chat/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        User user = new User("iTacco", "u0dA6Mr2");
-        Chat chat = new Chat(user);
+        /*User user = new User("iTacco", "u0dA6Mr2");
+        Chat chat = new Chat(user);*/
 
-        /*
+
 
         // Create a new chat object
         Chat chat = new Chat();
         // Login to chat. Username and password will be asked after this method call
-        chat.loginToChat();*/
+        chat.loginToChat();
     }
 }
